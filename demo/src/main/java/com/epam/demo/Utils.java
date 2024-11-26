d429b4c750d10329394cb80af74d2e1617fd99c9
xden78 <venera.opt@gmail.com>
2024-11-26 20:12:17 +0300

    1

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        int count = 0;
+        for (String arg : args) {
+            if (StringUtils.isPositiveNumber(arg)) count++;
+        }
+
+        return count == args.size();
     }
 }
